2445236d7e1bf56a8d468986f04cf78a1de63733
ebarhami <ensof.intern@stoqo.com>
2019-11-17 20:36:19 +0700

    Add split and predict data

--- dbscan.py
+++ dbscan.py
@@ -1,159 +1,118 @@
+class DBSCAN:
+    def __init__(self, min_pts, epsilon):
+        self.min_pts = min_pts
+        self.epsilon = epsilon
+        self.NOT_VISITED_LABEL = -99
+        self.NOISE_LABEL = -1
 
-import numpy
+    def label_dbscan(self, data):
+        labels = [self.NOT_VISITED_LABEL]*len(data) 
+        cluster = 0
 
-NOT_VISITED_LABEL = -99
-NOISE_LABEL = -1
+        for datum_idx in range(0, len(data)):
+            if not (labels[datum_idx] == self.NOT_VISITED_LABEL):
+                continue
 
-def dbscan(data, min_pts, epsilon):
-    """
-    Cluster the dataset `D` using the DBSCAN algorithm.
-    
-    MyDBSCAN takes a dataset `D` (a list of vectors), a threshold distance
-    `eps`, and a required number of points `MinPts`.
-    
-    It will return a list of cluster labels. The label -1 means noise, and then
-    the clusters are numbered starting from 1.
-    """
-    
-    # This list will hold the final cluster assignment for each point in D.
-    # There are two reserved values:
-    #    -1 - Indicates a noise point
-    #     0 - Means the point hasn't been considered yet.
-    # Initially all labels are 0.    
-    labels = [NOT_VISITED_LABEL]*len(data)
+            neighbors = self.neighbor_points(data, datum_idx)
 
-    # C is the ID of the current cluster.    
-    cluster = 0
-    
-    # This outer loop is just responsible for picking new seed points--a point
-    # from which to grow a new cluster.
-    # Once a valid seed point is found, a new cluster is created, and the 
-    # cluster growth is all handled by the 'expandCluster' routine.
-    
-    # For each point P in the Dataset D...
-    # ('P' is the index of the datapoint, rather than the datapoint itself.)
-    for datum_idx in range(0, len(data)):
-    
-        # Only points that have not already been claimed can be picked as new 
-        # seed points.    
-        # If the point's label is not 0, continue to the next point.
-        if not (labels[datum_idx] == NOT_VISITED_LABEL):
-           continue
-        
-        # Find all of P's neighboring points.
-        neighbors = neighbor_points(data, datum_idx, epsilon)
-        
-        # If the number is below MinPts, this point is noise. 
-        # This is the only condition under which a point is labeled 
-        # NOISE--when it's not a valid seed point. A NOISE point may later 
-        # be picked up by another cluster as a boundary point (this is the only
-        # condition under which a cluster label can change--from NOISE to 
-        # something else).
-        if len(neighbors) < min_pts:
-            labels[datum_idx] = NOISE_LABEL
-        # Otherwise, if there are at least MinPts nearby, use this point as the 
-        # seed for a new cluster.    
-        else: 
-           flood_fill(data, labels, datum_idx, neighbors, cluster, epsilon, min_pts)
-           cluster += 1
-    
-    # All data has been clustered!
-    return labels
+            if len(neighbors) < self.min_pts:
+                labels[datum_idx] = self.NOISE_LABEL
+            else: 
+                self.flood_fill(data, labels, datum_idx, neighbors, cluster)
+                cluster += 1
+
+        return labels
 
 
-def flood_fill(data, labels, datum_idx, neighbors, cluster, epsilon, min_pts):
-    """
-    Grow a new cluster with label `C` from the seed point `P`.
-    
-    This function searches through the dataset to find all points that belong
-    to this new cluster. When this function returns, cluster `C` is complete.
-    
-    Parameters:
-      `D`      - The dataset (a list of vectors)
-      `labels` - List storing the cluster labels for all dataset points
-      `P`      - Index of the seed point for this new cluster
-      `NeighborPts` - All of the neighbors of `P`
-      `C`      - The label for this new cluster.  
-      `eps`    - Threshold distance
-      `MinPts` - Minimum required number of neighbors
-    """
-
-    # Assign the cluster label to the seed point.
-    labels[datum_idx] = cluster
-    
-    # Look at each neighbor of P (neighbors are referred to as Pn). 
-    # NeighborPts will be used as a FIFO queue of points to search--that is, it
-    # will grow as we discover new branch points for the cluster. The FIFO
-    # behavior is accomplished by using a while-loop rather than a for-loop.
-    # In NeighborPts, the points are represented by their index in the original
-    # dataset.
-    for i in range(len(neighbors)):    
-        # Get the next point from the queue.        
-        neighbor = neighbors[i]
-        # If Pn was labelled NOISE during the seed search, then we
-        # know it's not a branch point (it doesn't have enough neighbors), so
-        # make it a leaf point of cluster C and move on.
-        if labels[neighbor] == NOISE_LABEL:
-           labels[neighbor] = cluster
-        
-        # Otherwise, if Pn isn't already claimed, claim it as part of C.
-        elif labels[neighbor] == NOT_VISITED_LABEL:
-            # Add Pn to cluster C (Assign cluster label C).
-            labels[neighbor] = cluster
+    def flood_fill(self, data, labels, datum_idx, neighbors, cluster):
+        labels[datum_idx] = cluster
+        i = 0
+        while i < len(neighbors):           
+            neighbor = neighbors[i]
             
-            # Find all the neighbors of Pn
-            new_neighbors = neighbor_points(data, neighbor, epsilon)
-            
-            # If Pn has at least MinPts neighbors, it's a branch point!
-            # Add all of its neighbors to the FIFO queue to be searched. 
-            if len(new_neighbors) >= min_pts:
-                neighbors = neighbors + new_neighbors
-            # If Pn *doesn't* have enough neighbors, then it's a leaf point.
-            # Don't queue up it's neighbors as expansion points.
-            #else:
-                # Do nothing                
-                #NeighborPts = NeighborPts               
-        
-        # Advance to the next point in the FIFO queue.        
-        i = i + 1
+            if labels[neighbor] == self.NOISE_LABEL:
+                labels[neighbor] = cluster
+            elif labels[neighbor] == self.NOT_VISITED_LABEL:
+                labels[neighbor] = cluster
+                new_neighbors = self.neighbor_points(data, neighbor)
+                if len(new_neighbors) >= self.min_pts:
+                    neighbors = neighbors + new_neighbors
+                    
+            i = i + 1
+
+
+    def neighbor_points(self, data, datum_idx):
+        neighbors = []
+
+        for curr_idx in range(0, len(data)):
+            if self.find_euclidean_distance(data[datum_idx], data[curr_idx]) < self.epsilon:
+                neighbors.append(curr_idx)
+            # print(find_euclidean_distance(data[datum_idx], data[curr_idx]))
+
+        return neighbors
+
+    def find_euclidean_distance(self, a, b):
+        dist_square = 0
+        for i in range(len(a)):
+            dist_square = dist_square + ((a[i] - b[i])**2)
+
+        return dist_square**(.5)
+
+
+import pandas as pd
+import numpy as np
+
+
+def pre_process(path_file):
+    df = pd.read_csv(path_file, sep=',', names=['A1', 'A2', 'A3', 'A4', 'kelas'])
     
-    # We've finished growing cluster C!
-
-
-def neighbor_points(data, datum_idx, epsilon):
-    """
-    Find all points in dataset `D` within distance `eps` of point `P`.
+    obj_df = df.select_dtypes(include=['object']).copy()
+    obj_df['kelas'] = obj_df['kelas'].astype('category')
+    obj_df['kelas'] = obj_df['kelas'].cat.codes
     
-    This function calculates the distance between a point P and every other 
-    point in the dataset, and then returns only those points which are within a
-    threshold distance `eps`.
-    """
-    neighbors = []
+    df['kelas'] = obj_df['kelas']
     
-    # For each point in the dataset...
-    for curr_idx in range(0, len(data)):
-        
-        # If the distance is below the threshold, add it to the neighbors list.
-        if find_euclidean_distance(data[datum_idx], data[curr_idx]) < epsilon:
-           neighbors.append(curr_idx)
-        # print(find_euclidean_distance(data[datum_idx], data[curr_idx]))
-            
-    return neighbors
+    return df
 
-def find_euclidean_distance(a, b):
-    dist_square = 0
-    for i in range(len(a)):
-        dist_square = dist_square + ((a[i] - b[i])**2)
-    
-    return dist_square**(.5)
+df = pre_process('iris.data')
 
-data = [
-    [5.1,3.5,1.4,0.2],
-    [4.9,3.0,1.4,0.2],
-    [4.7,3.2,1.3,0.2],
-    [4.6,3.1,1.5,0.2],
-    [5.0,3.6,1.4,0.2]
-]
 
-clusters = dbscan(data, epsilon=1, min_pts=3)
-print(clusters)
+X = df.drop(['kelas'], axis = 1)
+y = df['kelas']
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+X_train = np.array(X_train)
+X_test = np.array(X_test)
+y_train = np.array(y_train)
+y_test = np.array(y_test)
+
+X_train.shape
+
+
+
+
+dbscan = DBSCAN(min_pts = 5, epsilon = .9)
+labels = dbscan.label_dbscan(X_train)
+print(len(labels))
+print(labels)
+
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+
+x = X_train[:,0]
+y = X_train[:,1]
+z = X_train[:,2]
+t = X_train[:,3]
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(z, y, t, s=10)
+
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+plt.show()
